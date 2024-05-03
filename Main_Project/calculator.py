cb93c9c3a2da98907972cb223448ca3006e15212
Andy <habegefmail.com>
2024-05-03 12:25:02 +0200

    Added more parameters

--- Main_Project/calculator.py
+++ Main_Project/calculator.py
@@ -28,45 +28,75 @@ def calculator_main():
     calcapp.geometry("400x150")
 
 
-    dp_label = ctk.CTkLabel(calcapp, text="Delta impuls:") #delta impuls
+    dp_label = ctk.CTkLabel(calcapp, text="Delta Impuls:") #delta impuls
     dp_label.grid(row=0, column=0)
     dp_entry = ctk.CTkEntry(calcapp)
     dp_entry.grid(row=0, column=1)
 
-    dt_label = ctk.CTkLabel(calcapp, text="dt:")
-    dt_label.grid(row=1, column=0)
+    ve_label = ctk.CTkLabel(calcapp, text="Geschwindigkeit:") #Geschwindigkeit
+    ve_label.grid(row=1, column=0)
+    ve_entry = ctk.CTkEntry(calcapp)
+    ve_entry.grid(row=1, column=1)
+
+    dm_label = ctk.CTkLabel(calcapp, text="Delta Masse:") #delta masse
+    dm_label.grid(row=2, column=0)
+    dm_entry = ctk.CTkEntry(calcapp)
+    dm_entry.grid(row=2, column=1)
+
+    dt_label = ctk.CTkLabel(calcapp, text="Delta Zeit:") #delta zeit
+    dt_label.grid(row=3, column=0)
     dt_entry = ctk.CTkEntry(calcapp)
-    dt_entry.grid(row=1, column=1)
+    dt_entry.grid(row=3, column=1)
 
-    v0_label = ctk.CTkLabel(calcapp, text="v0:")
-    v0_label.grid(row=2, column=0)
-    v0_entry = ctk.CTkEntry(calcapp)
-    v0_entry.grid(row=2, column=1)
-
-    rho_label = ctk.CTkLabel(calcapp, text="rho:")
-    rho_label.grid(row=3, column=0)
-    rho_entry = ctk.CTkEntry(calcapp)
-    rho_entry.grid(row=3, column=1)
-
-    A_label = ctk.CTkLabel(calcapp, text="A:")
-    A_label.grid(row=4, column=0)
-    A_entry = ctk.CTkEntry(calcapp)
-    A_entry.grid(row=4, column=1)
+    cw_label = ctk.CTkLabel(calcapp, text="Strömungswiderstandsbeiwert:") #Strömungswiderstandsbeiwert
+    cw_label.grid(row=4, column=0)
+    cw_entry = ctk.CTkEntry(calcapp)
+    cw_entry.grid(row=4, column=1)
 
     cw_label = ctk.CTkLabel(calcapp, text="cw:")
     cw_label.grid(row=5, column=0)
     cw_entry = ctk.CTkEntry(calcapp)
     cw_entry.grid(row=5, column=1)
 
-    m_label = ctk.CTkLabel(calcapp, text="m:")
-    m_label.grid(row=6, column=0)
-    m_entry = ctk.CTkEntry(calcapp)
-    m_entry.grid(row=6, column=1)
+    p_label = ctk.CTkLabel(calcapp, text="Luftdichte:") #Luftdichte
+    p_label.grid(row=6, column=0)
+    p_entry = ctk.CTkEntry(calcapp)
+    p_entry.grid(row=6, column=1)
 
-    g_label = ctk.CTkLabel(calcapp, text="g:")
-    g_label.grid(row=7, column=0)
+    A_label = ctk.CTkLabel(calcapp, text="Frontfläche:") #Frontfläche
+    A_label.grid(row=8, column=0)
+    A_entry = ctk.CTkEntry(calcapp)
+    A_entry.grid(row=8, column=1)
+
+    v_label = ctk.CTkLabel(calcapp, text="Geschwindigkeit:") #Geschwindigkeit
+    v_label.grid(row=9, column=0)
+    v_entry = ctk.CTkEntry(calcapp)
+    v_entry.grid(row=9, column=1)
+
+    h_label = ctk.CTkLabel(calcapp, text="Höhe:") #Höhe
+    h_label.grid(row=10, column=0)
+    h_entry = ctk.CTkEntry(calcapp)
+    h_entry.grid(row=10, column=1)
+
+    p0_label = ctk.CTkLabel(calcapp, text="Luftdichte:") #Luftdichte auf Meereshöhe
+    p0_label.grid(row=11, column=0)
+    p0_entry = ctk.CTkEntry(calcapp)
+    p0_entry.grid(row=11, column=1)
+
+    g_label = ctk.CTkLabel(calcapp, text="Erdbeschleunigung:") #Erdbeschleunigung
+    g_label.grid(row=12, column=0)
     g_entry = ctk.CTkEntry(calcapp)
-    g_entry.grid(row=7, column=1)
+    g_entry.grid(row=12, column=1)
+
+    M_label = ctk.CTkLabel(calcapp, text="Die mittlere molare Masse der Luft:") #M die mittlere molare Masse der Luft
+    M_label.grid(row=13, column=0)
+    M_entry = ctk.CTkEntry(calcapp)
+    M_entry.grid(row=13, column=1)
+
+    _label = ctk.CTkLabel(calcapp, text=":") #
+    _label.grid(row=13, column=0)
+    _entry = ctk.CTkEntry(calcapp)
+    _entry.grid(row=13, column=1)
 
 
 
@@ -77,8 +107,8 @@ def calculator_main():
 
 
 
-    calcbutton.grid(row = 8, column=1)
-    button.grid(row = 9, column=1)
+    calcbutton.grid(row = 14, column=1)
+    button.grid(row = 15, column=1)
 
     calcapp.mainloop()
 
